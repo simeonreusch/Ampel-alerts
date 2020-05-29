d6a949847ca17bb936b6ba92df8007c77351ec7c
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 15:44:28 +0200

    AlertProcessingModel: v0.7 parameters

--- ampel/model/AlertProcessingModel.py
+++ ampel/model/AlertProcessingModel.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/model/t0/APChanModel.py
+# File              : Ampel-alerts/ampel/model/AlertProcessingModel.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.10.2019
-# Last Modified Date: 03.11.2019
+# Last Modified Date: 17.05.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, Optional
-from ampel.types import ChannelId
-from ampel.model.AmpelBaseModel import AmpelBaseModel
-from ampel.model.t0.T0FilterModel import T0FilterModel
+from typing import Union, Optional, List, Dict, Literal
+from ampel.type import ChannelId
+from ampel.model.AmpelStrictModel import AmpelStrictModel
+from ampel.model.T0AddModel import T0AddModel
+from ampel.model.T1CombineModel import T1CombineModel
+from ampel.model.T2ComputeModel import T2ComputeModel
+from ampel.model.DataUnitModel import DataUnitModel
+from ampel.model.AliasedDataUnitModel import AliasedDataUnitModel
+from ampel.model.AutoStockMatchModel import AutoStockMatchModel
 
-class APChanModel(AmpelBaseModel):
-	""" """
-	name: ChannelId
+class AliasedFilterModel(AliasedDataUnitModel):
+	reject: Optional[Dict[Literal['log', 'register'], Union[AliasedDataUnitModel, DataUnitModel]]]
+
+class FilterModel(DataUnitModel):
+	reject: Optional[Dict[Literal['log', 'register'], Union[AliasedDataUnitModel, DataUnitModel]]]
+
+class AlertProcessingModel(AmpelStrictModel):
+	channel: ChannelId
 	dist_name: Optional[str]
-	auto_complete: Union[bool, str]
-	t0_add: T0FilterModel
+	filter: Union[FilterModel, AliasedFilterModel]
+	stock_match: Optional[AutoStockMatchModel]
+	t0_add: T0AddModel
+	t1_combine: Optional[List[T1CombineModel]]
+	t2_compute: Optional[T2ComputeModel]
+	stock_update: Union[DataUnitModel, AliasedDataUnitModel]

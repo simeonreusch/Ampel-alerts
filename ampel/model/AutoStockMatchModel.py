706068f74e319e7106eef705468ce8f5463bd1e5
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 15:49:28 +0200

    New class: AutoStockMatchModel

---
+++ ampel/model/AutoStockMatchModel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-alerts/ampel/model/AutoStockMatchModel.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 05.05.2020
+# Last Modified Date: 05.05.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Literal
+from ampel.model.AmpelStrictModel import AmpelStrictModel
+
+class AutoStockMatchModel(AmpelStrictModel):
+	filter: Literal['bypass', 'overrule']
+	update_rej: bool = True
+	retro_complete: bool

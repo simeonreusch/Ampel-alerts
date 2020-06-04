b689560a7298c489d99f6dc241b7289a19f1cf49
vbrinnel <valery.brinnel@gmail.com>
2020-06-04 12:25:47 +0200

    T2IngestModel: allow None config

--- ampel/model/T2IngestModel.py
+++ ampel/model/T2IngestModel.py
@@ -4,16 +4,15 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.03.2020
-# Last Modified Date: 11.05.2020
+# Last Modified Date: 03.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Any, Dict, List, Union, Optional
 from ampel.model.AmpelStrictModel import AmpelStrictModel
-from ampel.util.mappings import build_unsafe_short_dict_id
 
 class T2IngestModel(AmpelStrictModel):
 
 	unit: str
-	config: int = build_unsafe_short_dict_id(None)
+	config: Optional[int]
 	ingest: Optional[Dict[str, Any]]
 	group: Union[int, List[int]] = []

3d11106832d0fae599ef878199dedf9c7089450f
Jakob van Santen <jvansanten@gmail.com>
2020-08-28 14:38:50 +0200

    TarAlertLoader: make shiny for mypy

--- ampel/alert/load/TarAlertLoader.py
+++ ampel/alert/load/TarAlertLoader.py
@@ -8,7 +8,7 @@
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import tarfile
-from typing import Optional, BinaryIO
+from typing import Optional, IO
 from ampel.log.AmpelLogger import AmpelLogger
 
 
@@ -17,13 +17,13 @@ class TarAlertLoader:
 	"""
 
 	def __init__(
-		self, tar_path: str = None, file_obj: BinaryIO = None, start: int = 0,
+		self, tar_path: str = None, file_obj: IO[bytes] = None, start: int = 0,
 		tar_mode: str = 'r:gz', logger: AmpelLogger = None
 	):
 		"""
 		"""
 		self.logger = AmpelLogger.get_logger() if logger is None else logger
-		self.chained_tal = None
+		self.chained_tal: Optional['TarAlertLoader'] = None
 
 		if file_obj is not None:
 			self.tar_file = tarfile.open(fileobj=file_obj, mode=tar_mode)
@@ -45,7 +45,7 @@ class TarAlertLoader:
 		return self
 
 
-	def __next__(self) -> tarfile.ExFileObject:
+	def __next__(self) -> IO[bytes]:
 		"""
 		FYI:
 		from io import IOBase
@@ -55,7 +55,8 @@ class TarAlertLoader:
 		Out[]: True
 		"""
 		# Free memory
-		self.tar_file.members.clear()
+		# Note that TarFile returns its members as a mutable list
+		self.tar_file.getmembers().clear()
 
 		if self.chained_tal is not None:
 			file_obj = self.get_chained_next()
@@ -79,21 +80,25 @@ class TarAlertLoader:
 
 			# extractfile returns a file like obj
 			file_obj = self.tar_file.extractfile(tar_info)
+			assert file_obj is not None
 
 			# Handle tars with nested tars
 			if tar_info.name.endswith('.tar.gz'):
 				self.chained_tal = TarAlertLoader(file_obj=file_obj)
-				file_obj = self.get_chained_next()
-				return file_obj if file_obj is not None else next(self)
+				if (subfile_obj := self.get_chained_next()) is not None:
+					return subfile_obj
+				else:
+					return next(self)
 
 			return file_obj
 
 		return next(self)
 
 
-	def get_chained_next(self) -> Optional[tarfile.ExFileObject]:
+	def get_chained_next(self) -> Optional[IO[bytes]]:
 		"""
 		"""
+		assert self.chained_tal is not None
 		file_obj = next(self.chained_tal, None)
 		if file_obj is None:
 			self.chained_tal = None

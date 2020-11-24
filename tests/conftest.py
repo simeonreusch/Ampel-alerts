db8f18f402361df549df037dd1052f68f08bcdca
Jakob van Santen <jvansanten@gmail.com>
2020-11-24 20:28:58 +0100

    test: move ingester fixtures to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,22 @@ from pathlib import Path
 import mongomock
 import pytest
 
+from ampel.demo.unit.base.DemoPointT2Unit import DemoPointT2Unit
 from ampel.dev.DevAmpelContext import DevAmpelContext
+from ampel.ingest.PointT2Ingester import PointT2Ingester
+from ampel.ingest.StockIngester import StockIngester
+from ampel.ingest.StockT2Ingester import StockT2Ingester
+from ampel.model.AlertProcessorDirective import AlertProcessorDirective
+
+from .dummy_units import (
+    DummyAlertContentIngester,
+    DummyCompoundIngester,
+    DummyExtendedCompoundIngester,
+    DummyStateT2Ingester,
+    DummyStateT2Unit,
+    DummyStockT2Unit,
+)
+
 
 @pytest.fixture
 def patch_mongo(monkeypatch):
@@ -13,3 +28,64 @@ def patch_mongo(monkeypatch):
 @pytest.fixture
 def dev_context(patch_mongo):
     return DevAmpelContext.load(Path(__file__).parent / "testing-config.yaml",)
+
+
+@pytest.fixture
+def legacy_directive():
+    return AlertProcessorDirective(
+        **{
+            "channel": "TEST_CHANNEL",
+            "stock_update": {"unit": StockIngester},
+            "t0_add": {
+                "ingester": DummyAlertContentIngester,
+                "t1_combine": [
+                    {
+                        "ingester": DummyCompoundIngester,
+                        "t2_compute": {
+                            "ingester": DummyStateT2Ingester,
+                            "units": [{"unit": DummyStateT2Unit}],
+                        },
+                    }
+                ],
+                "t2_compute": {
+                    "ingester": PointT2Ingester,
+                    "units": [{"unit": DemoPointT2Unit}],
+                },
+            },
+            "t2_compute": {
+                "ingester": StockT2Ingester,
+                "units": [{"unit": DummyStockT2Unit}],
+            },
+        }
+    )
+
+
+@pytest.fixture
+def standalone_t1_directive():
+    return AlertProcessorDirective(
+        **{
+            "channel": "TEST_CHANNEL",
+            "stock_update": {"unit": StockIngester},
+            "t0_add": {
+                "ingester": DummyAlertContentIngester,
+                "t1_combine": [
+                    {
+                        "ingester": DummyCompoundIngester,
+                        "t2_compute": {
+                            "ingester": DummyStateT2Ingester,
+                            "units": [{"unit": DummyStateT2Unit}],
+                        },
+                    }
+                ],
+            },
+            "t1_combine": [
+                {
+                    "ingester": DummyExtendedCompoundIngester,
+                    "t2_compute": {
+                        "ingester": DummyStateT2Ingester,
+                        "units": [{"unit": DummyStateT2Unit}],
+                    },
+                }
+            ],
+        }
+    )
--- tests/test_IngestionHandler.py
+++ tests/test_IngestionHandler.py
@@ -3,22 +3,12 @@ import pytest
 from ampel.alert.AmpelAlert import AmpelAlert
 from ampel.alert.IngestionHandler import IngestionHandler
 from ampel.db.DBUpdatesBuffer import DBUpdatesBuffer
-from ampel.demo.unit.base.DemoPointT2Unit import DemoPointT2Unit
-from ampel.ingest.PointT2Ingester import PointT2Ingester
 from ampel.ingest.StockIngester import StockIngester
-from ampel.ingest.StockT2Ingester import StockT2Ingester
 from ampel.log.AmpelLogger import AmpelLogger, DEBUG
 from ampel.log.LogsBufferDict import LogsBufferDict
 from ampel.model.AlertProcessorDirective import AlertProcessorDirective
 
-from .dummy_units import (
-    DummyAlertContentIngester,
-    DummyCompoundIngester,
-    DummyExtendedCompoundIngester,
-    DummyStateT2Ingester,
-    DummyStateT2Unit,
-    DummyStockT2Unit,
-)
+from .dummy_units import DummyAlertContentIngester
 
 
 def get_handler(context, directives):
@@ -62,32 +52,8 @@ def test_minimal_directive(dev_context):
     assert stock.count_documents({"_id": "stockystock"}) == 1
 
 
-def test_legacy_directive(dev_context):
-    directive = {
-        "channel": "TEST_CHANNEL",
-        "stock_update": {"unit": StockIngester},
-        "t0_add": {
-            "ingester": DummyAlertContentIngester,
-            "t1_combine": [
-                {
-                    "ingester": DummyCompoundIngester,
-                    "t2_compute": {
-                        "ingester": DummyStateT2Ingester,
-                        "units": [{"unit": DummyStateT2Unit}],
-                    },
-                }
-            ],
-            "t2_compute": {
-                "ingester": PointT2Ingester,
-                "units": [{"unit": DemoPointT2Unit}],
-            },
-        },
-        "t2_compute": {
-            "ingester": StockT2Ingester,
-            "units": [{"unit": DummyStockT2Unit}],
-        },
-    }
-    handler = get_handler(dev_context, [AlertProcessorDirective(**directive)])
+def test_legacy_directive(dev_context, legacy_directive):
+    handler = get_handler(dev_context, [legacy_directive])
     assert isinstance(handler.stock_ingester, StockIngester)
     assert isinstance(handler.datapoint_ingester, DummyAlertContentIngester)
 
@@ -122,41 +88,11 @@ def test_legacy_directive(dev_context):
         assert len(docs[i]["link"]) == 1
 
 
-@pytest.fixture
-def standalone_t1_directive():
-    return {
-        "channel": "TEST_CHANNEL",
-        "stock_update": {"unit": StockIngester},
-        "t0_add": {
-            "ingester": DummyAlertContentIngester,
-            "t1_combine": [
-                {
-                    "ingester": DummyCompoundIngester,
-                    "t2_compute": {
-                        "ingester": DummyStateT2Ingester,
-                        "units": [{"unit": DummyStateT2Unit}],
-                    },
-                }
-            ],
-        },
-        "t1_combine": [
-            {
-                "ingester": DummyExtendedCompoundIngester,
-                "t2_compute": {
-                    "ingester": DummyStateT2Ingester,
-                    "units": [{"unit": DummyStateT2Unit}],
-                },
-            }
-        ],
-    }
-
-
 def test_standalone_t1_directive(dev_context, standalone_t1_directive):
     """
     Extended history is created if a channel requests it
     """
-    directive = standalone_t1_directive
-    handler = get_handler(dev_context, [AlertProcessorDirective(**directive)])
+    handler = get_handler(dev_context, [standalone_t1_directive])
 
     handler.ingest(
         AmpelAlert(id="alert", stock_id="stockystock", dps=[{}]),
@@ -185,8 +121,8 @@ def test_standalone_t1_channel_dispatch(dev_context, standalone_t1_directive):
     """
     Extended history compounds are created only for channels that request it
     """
-    long_channel = {**standalone_t1_directive, **{"channel": "LONG_CHANNEL"}}
-    short_channel = {**standalone_t1_directive, **{"channel": "SHORT_CHANNEL"}}
+    long_channel = {**standalone_t1_directive.dict(), **{"channel": "LONG_CHANNEL"}}
+    short_channel = {**standalone_t1_directive.dict(), **{"channel": "SHORT_CHANNEL"}}
     del short_channel["t1_combine"]
 
     handler = get_handler(
@@ -221,8 +157,8 @@ def test_standalone_t1_elision(dev_context, standalone_t1_directive):
     """
     Extended history points are skipped when only short channels pass
     """
-    long_channel = {**standalone_t1_directive, **{"channel": "LONG_CHANNEL"}}
-    short_channel = {**standalone_t1_directive, **{"channel": "SHORT_CHANNEL"}}
+    long_channel = {**standalone_t1_directive.dict(), **{"channel": "LONG_CHANNEL"}}
+    short_channel = {**standalone_t1_directive.dict(), **{"channel": "SHORT_CHANNEL"}}
     del short_channel["t1_combine"]
 
     handler = get_handler(

cc82f5d5952ac157d1f94a574904ede2148baa85
vbrinnel <valery.brinnel@gmail.com>
2021-08-10 17:53:00 +0200

    test_AlertProcessor: incomplete v0.8 fix.
    - add tests from v0.7 commit https://github.com/AmpelProject/Ampel-alerts/commit/d2163e95fecf77a0a079a56af9ecb02b9e6be16d
    - rudimentary and incomplete v0.8, most unit test will still fail

--- tests/test_AlertProcessor.py
+++ tests/test_AlertProcessor.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-alerts/tests/test_AlertProcessor.py
+# License           : BSD-3-Clause
+# Author            : jvs
+# Date              : Unspecified
+# Last Modified Date: 05.08.2021
+# Last Modified By  : vb
+
+import os, signal, time, threading
 from contextlib import contextmanager
 
 from ampel.alert.AlertConsumer import AlertConsumer
+from ampel.alert.AlertConsumerError import AlertConsumerError
 from ampel.alert.AmpelAlert import AmpelAlert
 from ampel.alert.filter.BasicMultiFilter import BasicMultiFilter
 from ampel.dev.UnitTestAlertSupplier import UnitTestAlertSupplier
 from ampel.metrics.AmpelMetricsRegistry import AmpelMetricsRegistry
-from ampel.model.AlertConsumerDirective import FilterModel
+from ampel.model.ingest.FilterModel import FilterModel
 
 
 @contextmanager
@@ -32,6 +43,7 @@ def test_no_filter(dev_context, legacy_directive):
         ap = AlertConsumer(
             context=dev_context,
             process_name="ap",
+            shaper = "NoShaper",
             directives=[legacy_directive],
             supplier=UnitTestAlertSupplier(
                 alerts=[AmpelAlert(id="alert", stock_id="stockystock", dps=[])]
@@ -50,7 +62,15 @@ def test_no_filter(dev_context, legacy_directive):
         ]
         == 1
     )
-    assert stats[("ampel_alertprocessor_time_seconds_sum", (("section", "ingest"),),)] > 0
+    assert (
+        stats[
+            (
+                "ampel_alertprocessor_time_seconds_sum",
+                (("section", "ingest"),),
+            )
+        ]
+        > 0
+    )
     assert (
         stats[
             (
@@ -82,6 +102,7 @@ def test_with_filter(dev_context, legacy_directive):
         ap = AlertConsumer(
             context=dev_context,
             process_name="ap",
+            shaper = "NoShaper",
             directives=[legacy_directive],
             supplier=UnitTestAlertSupplier(
                 alerts=[AmpelAlert(id="alert", stock_id="stockystock", dps=[])]
@@ -110,3 +131,56 @@ def test_with_filter(dev_context, legacy_directive):
         ]
         > 0
     )
+
+
+def test_suspend_in_supplier(dev_context, legacy_directive=None):
+
+    # simulate a producer that blocks while waiting fo upstream input
+    class BlockingAlertSupplier(UnitTestAlertSupplier):
+        def __iter__(self):
+            for el in super().__iter__():
+                print("sleep 3")
+                time.sleep(3)
+                yield el
+
+    dev_context.register_unit(BlockingAlertSupplier)
+    ap = AlertConsumer(
+        context = dev_context,
+        process_name = "ap",
+        shaper = "NoShaper",
+        directives = [legacy_directive or {"channel": "CHAN1", "filter": None}],
+        supplier = {
+            "unit": "BlockingAlertSupplier",
+            "config": {
+                "alerts": [AmpelAlert(id="alert", stock_id="stockystock", dps=[])]
+            }
+        }
+    )
+
+    def alarm():
+        time.sleep(0.5)
+        os.kill(os.getpid(), signal.SIGINT)
+
+    t = threading.Thread(target=alarm)
+    t.start()
+    t0 = time.time()
+    assert ap.run() == 0, "AP suspended before first alert was processed"
+    assert time.time() - t0 < 2, "AP suspended before supplier timed out"
+    t.join()
+
+
+def test_suspend_in_critical_section(dev_context, legacy_directive, monkeypatch):
+    monkeypatch.setattr(
+        BasicMultiFilter, "process", lambda *args: os.kill(os.getpid(), signal.SIGINT)
+    )
+    legacy_directive.filter = FilterModel(unit=BasicMultiFilter, config={"filters": []})
+    ap = AlertConsumer(
+        context=dev_context,
+        process_name="ap",
+        directives=[legacy_directive],
+        supplier=UnitTestAlertSupplier(
+            alerts=[AmpelAlert(id="alert", stock_id="stockystock", dps=[])]
+        ),
+    )
+    assert ap.run() == 1, "AP successfully processes alert"
+    assert ap._cancel_run == AlertConsumerError.SIGINT

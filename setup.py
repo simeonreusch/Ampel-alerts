d5402137b79b39ad0843d8d6ab9079fe86f39097
vbrinnel <valery.brinnel@gmail.com>
2021-11-25 14:26:24 +0100

    Bump version: 0.8.1a2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ package_data = {
 
 setup(
     name = 'ampel-alerts',
-    version = '0.8.1.alpha-1',
+    version = '0.8.1.alpha-2',
     description = 'Asynchronous and Modular Platform with Execution Layers',
     author = 'Valery Brinnel',
     maintainer = 'Jakob van Santen',
